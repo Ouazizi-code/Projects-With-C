c958fdcfadfdc6b644c703b079f2d41703130f57
ouazizi <ouazizi@gmail.com>
2024-06-25 19:23:57 +0100

    add avreage project

---
+++ numbers-average/README.md
@@ -0,0 +1,4 @@
+<h1>average of numbers</h1>
+<h3>this repo about some calculation and math in c</h3>
+i used the for loop to calculate the sum of arra<br>
+and how to take nums from user and store them into array <br>
---
+++ numbers-average/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+
+
+
+
+
+// ask user for length oh numbers and store them into array
+int average(void){
+
+    int length;
+    printf("how many numbers ? :");
+    scanf("%i",&length);
+    int numbers[length];
+
+    // use for loop to store the numbers in arr
+    for(int i = 0; i < length; i++){
+        printf("enter the %i number :",i);
+        scanf("%i",&numbers[i]);
+        
+    }
+    
+    // print the array 
+    printf("the numbers are :");
+     for(int i=0; i< length; i++){
+        printf("%d,",numbers[i]);
+    }
+    puts("");
+
+    // calculate the average
+    int result =0;
+    for(int i = 0; i <length; i++){
+        result += numbers[i] ;
+    }
+    printf("the average is :%.2f\n",result/(float)length);
+
+}
+
+
+
+
+
+int main(){
+
+    puts("#===Hello average===#");
+    average();
+}
